fae7fbc625c8babb7998978b22727d185776ea1f
firas hamila <firas.hm.1996@gmail.com>
2025-02-24 16:04:06 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,37 @@ exports.getUsers = async (req, res) => {
     const users = await User.find();
     res.status(200).json({
       message: "Users Fetched !!!",
+      nbr: users.length,
       data: { users },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      message: "User Fetched !!!",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      message: "User Fetched !!!",
     });
   } catch (error) {
     res.status(400).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,13 @@ const {
   createUser,
   updateUser,
   getUsers,
+  getUser,
+  deleteUser,
 } = require("../controllers/userController");
 const Router = express.Router();
 Router.route("/").post(createUser);
 Router.route("/").get(getUsers);
+Router.route("/:id").get(getUser);
 Router.route("/:id").patch(updateUser);
+Router.route("/:id").delete(deleteUser);
 module.exports = Router;
